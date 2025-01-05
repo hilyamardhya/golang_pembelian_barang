391b59a156dceb2353c95a9361e3901b6faffd8a
Your Name <you@example.com>
2025-01-05 10:56:03 +0700

    penambahan table admin

--- project_sddp.sql
+++ project_sddp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2024 at 04:03 PM
+-- Generation Time: Jan 05, 2025 at 04:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `role`) VALUES
+(1, '[admin]', '[admin]', '[admin]');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `barang`
 --
 
@@ -39,11 +59,11 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`IdBarang`, `NamaBarang`, `Harga`, `Stok`) VALUES
-(1, 'Buku Tulis', 3000, 20),
-(2, 'Pulpen', 3000, 500),
-(3, 'Penghapus', 5000, 200),
-(4, 'Pensil', 3000, 500),
-(5, 'Tip Ex', 10000, 250);
+(1, 'Buku Tulis', 3000.00, 20),
+(2, 'Pulpen', 3000.00, 500),
+(3, 'Penghapus', 5000.00, 200),
+(4, 'Pensil', 3000.00, 500),
+(5, 'Tip Ex', 10000.00, 250);
 
 -- --------------------------------------------------------
 
@@ -56,14 +76,27 @@ CREATE TABLE `pembelian` (
   `Nama` varchar(50) DEFAULT NULL,
   `Barang` varchar(100) DEFAULT NULL,
   `Jumlah` int(11) DEFAULT NULL,
-  `TotalBayar` decimal(10, 2) DEFAULT NULL,
+  `TotalBayar` decimal(10,2) DEFAULT NULL,
   `IdBarang` int(11) NOT NULL,
-  `Tanggal` date DEFAULT CURDATE()
+  `Tanggal` date DEFAULT curdate()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembelian`
+--
+
+INSERT INTO `pembelian` (`IdPembelian`, `Nama`, `Barang`, `Jumlah`, `TotalBayar`, `IdBarang`, `Tanggal`) VALUES
+(13, 'alya', 'pulpen', 3, 9000.00, 2, '2024-12-21');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `barang`
@@ -79,24 +112,30 @@ ALTER TABLE `pembelian`
   ADD KEY `IdBarang` (`IdBarang`);
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for table `admin`
 --
+ALTER TABLE `admin`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `IdBarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `IdBarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `pembelian`
+-- AUTO_INCREMENT for table `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `IdPembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `IdPembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
 
 --
 -- Constraints for table `pembelian`
